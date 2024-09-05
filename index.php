529511c68b0a02e8c612b410b594d8a2ef4523f1
João Santos <joao@talkagency.com.au>
2024-09-05 05:52:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/routes.php';
+require_once __DIR__ . '/src/routes.php';
 
 SimpleRouter::start();
