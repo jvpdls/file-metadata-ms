78c141e5f227091a85c661b109459da687c1c7da
João Santos <joao@talkagency.com.au>
2024-09-05 05:52:02 -0300

    folders reorganization

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,9 @@
 
 use Pecee\SimpleRouter\SimpleRouter;
 
-require_once __DIR__ . '/api/fileAnalyser.php';
+require_once __DIR__ . '/api/fileAnalyzer.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader('src/templates');
 $twig = new \Twig\Environment($loader, [
     'debug' => true,
 ]);
@@ -13,7 +13,7 @@ SimpleRouter::get('/', function () use ($twig) {
     return $twig->render('pages/index.html.twig');
 });
 
-SimpleRouter::post('/api/analyse', function () {
+SimpleRouter::post('/api/analyze', function () {
     $file = $_FILES['file'] ?? null;
     $response = showFileMetadata($file);
     return $response;
